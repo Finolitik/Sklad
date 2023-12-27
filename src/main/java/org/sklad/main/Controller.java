2cf1c3d295890a016e9e8b421711985592190053
Dmitry <rct.kulaschi@bsu.by>
2023-12-27 19:04:04 +0300

    g

---
+++ src/main/java/org/sklad/main/Controller.java
@@ -0,0 +1,9 @@
+package org.sklad.main;
+
+import org.sklad.view.screen.ChoosingRoleFrame;
+
+public class Controller {
+    public Controller() {
+        ChoosingRoleFrame choosingRoleFrame = new ChoosingRoleFrame();
+    }
+}
